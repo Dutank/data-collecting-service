e1103e3f695fae4301c4790eb554e5bea68db13b
Duhyeong Lee <duhyeong.lee@ipoint-systems.de>
2020-02-25 15:39:51 +0100

    ixed typo

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"r
+    return "Hello World!"
 @app.route('/machinelearning')
 def machine_learning():
     retJson = {
